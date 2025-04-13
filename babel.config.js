8213bac816047d544690fd903527ca67a500e236
StevenWijaya11 <stvngw@gmail.com>
2025-04-13 14:02:14 +0800

    NYT-4: Set up module path aliases via babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,17 @@ module.exports = {
       moduleName: '@env',
       path: '.env',
     }],
+    ['module-resolver',{
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: [
+          { '@test': './__tests__/' },
+          { '@app': './src/app/' },
+          { '@core': './src/core/' },
+          { '@resources': './src/resources/' },
+          { '@ui': './src/ui/' },
+          { '@utils': './src/utils/' },
+        ],
+      }],
   ],
 };
