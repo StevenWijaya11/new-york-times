3ebece78c30f025376aee567a35ec57b92e1e4c8
StevenWijaya11 <stvngw@gmail.com>
2025-04-14 16:12:08 +0800

    NYT-7: Create axios instance and request interceptors

---
+++ src/core/services/axiosInstance.ts
@@ -0,0 +1,19 @@
+import { API_KEY, API_URL } from '@env';
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  timeout: 10000,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const url = new URL(config.url || '', config.baseURL);
+    url.searchParams.append('api-key', API_KEY);
+    config.url = url.toString();
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
